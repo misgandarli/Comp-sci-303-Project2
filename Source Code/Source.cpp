26c91471ed191b165ae2327f3b9ea37ab5c5bd12
Jeff Morgan <quicksh0t12@gmail.com>
2017-03-26 21:23:53 -0500

    Added Beginning Function of Recommended Books Algotirthm

--- Source Code/Source.cpp
+++ Source Code/Source.cpp
@@ -3,10 +3,24 @@
 #include "Rating.h"
 #include "Book.h"
 #include <cstdlib>
+#include <list>
+#include <iostream>
 
+using namespace std;
 
 
 void main()
 {
 	LibraryTree<Book> bookTree();
+}
+
+list<Book> RecommendBooks(LibraryTree<Book>& root, list<Customer>& customers, list<Book> recommended )
+{
+	recommended = {};
+	for (list<Customer>::iterator it = customers.begin(); it != customers.end(); it++)
+	{
+		//TODO
+	}
+	return recommended;
+
 }
